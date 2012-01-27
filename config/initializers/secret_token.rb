ff41b6d476cdf7fd2c0a8f0c8bb3c8fbe609a93c
Casey Li <casey.li@gmail.com>
2012-01-26 21:02:28 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Whatswrongwithpresentations::Application.config.secret_token = '82efb8a51e4bf0671bdfaebe43017f3f694c9f632ddca03c2e38da5dc9a6ff1332770113d93dbc1a211c8a8dff3bd63278ee58a47e4e3e7eb89d644fc39111f7'
