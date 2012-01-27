ae1c702145679295aae8c62fd757dd11a508be3f
Casey Li <casey.li@gmail.com>
2012-01-27 12:13:43 -0500

    Restyled the header a bit.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def logo
-     image_tag("logo.png", :alt => "WWWP", :width => "300px")
+     image_tag("logo.png", :alt => "WWWP", :class => "header_logo")
   end  
 
 
